b5d1c93867e9e66026b9ec6afe0f89a990a0d083
sunyue <814983809>
2019-01-17 10:45:02 +0800

    树形控件2

--- manage/src/views/organize/index.vue
+++ manage/src/views/organize/index.vue
@@ -1,16 +1,32 @@
 <template>
-  <h1>当前路径：{{this.$route.path}}</h1>
+  <div>
+    <h1>当前路径：{{this.$route.path}}</h1>
+    <div class="block">
+      <p>使用 scoped slot</p>
+      <el-tree
+        :data="data"
+        show-checkbox
+        node-key="id"
+        default-expand-all=false
+        :expand-on-click-node="false">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+          <span>{{ node.name }}</span>
+          <span>
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => append(data)">
+              Append
+            </el-button>
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => remove(node, data)">
+              Delete
+            </el-button>
+          </span>
+        </span>
+      </el-tree>
+    </div>
+  </div>
 </template>
-
-<script>
-export default {
-  data(){
-    return {
-      path: this.$route
-    }
-  },
-  created() {
-    console.log(this.$route);
-  },
-}
-</script>
